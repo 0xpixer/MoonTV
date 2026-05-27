5f2a3b7346e132e1580620bb5f550b52377ea1b5
0xpixer <arrow7440@gmail.com>
2026-05-27 20:27:18 +1000

    fix(play): constrain playback poster layout

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1390,7 +1390,7 @@ function PlayPageClient() {
           </div>
 
           <div
-            className={`grid gap-4 lg:h-[500px] xl:h-[650px] 2xl:h-[750px] transition-all duration-300 ease-in-out ${
+            className={`grid gap-4 lg:items-stretch transition-all duration-300 ease-in-out ${
               isEpisodeSelectorCollapsed
                 ? 'grid-cols-1'
                 : 'grid-cols-1 md:grid-cols-4'
@@ -1398,14 +1398,14 @@ function PlayPageClient() {
           >
             {/* 播放器 */}
             <div
-              className={`h-full transition-all duration-300 ease-in-out rounded-xl border border-white/0 dark:border-white/30 ${
+              className={`transition-all duration-300 ease-in-out rounded-xl border border-white/0 dark:border-white/30 ${
                 isEpisodeSelectorCollapsed ? 'col-span-1' : 'md:col-span-3'
               }`}
             >
-              <div className='relative w-full h-[300px] lg:h-full'>
+              <div className='relative w-full h-[260px] sm:h-[300px] lg:h-auto lg:aspect-video lg:max-h-[calc(100vh-12rem)] overflow-hidden rounded-xl bg-black'>
                 <video
                   ref={videoRef}
-                  className='bg-black w-full h-full rounded-xl overflow-hidden shadow-lg'
+                  className='bg-black w-full h-full object-contain rounded-xl overflow-hidden shadow-lg'
                   controls
                   playsInline
                   webkit-playsinline='true'
@@ -1455,7 +1455,7 @@ function PlayPageClient() {
 
             {/* 选集和换源 - 在移动端始终显示，在 lg 及以上可折叠 */}
             <div
-              className={`h-[300px] lg:h-full md:overflow-hidden transition-all duration-300 ease-in-out ${
+              className={`h-[300px] lg:h-auto lg:self-stretch md:overflow-hidden transition-all duration-300 ease-in-out ${
                 isEpisodeSelectorCollapsed
                   ? 'md:col-span-1 lg:hidden lg:opacity-0 lg:scale-95'
                   : 'md:col-span-1 lg:opacity-100 lg:scale-100'
